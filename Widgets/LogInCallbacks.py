8f4e0489eb5104df3448dfe124f5837e3fbb5fe5
noosmolegi <noosmolegi>
2008-09-24 13:50:38 +0000

    Added password recovery to the login dialog

--- Widgets/LogInCallbacks.py
+++ Widgets/LogInCallbacks.py
@@ -19,7 +19,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-import Webwidgets, traceback
+import Webwidgets, smtplib, traceback
 
 class LogIn(object):
     global_session = True
@@ -27,6 +27,25 @@ class LogIn(object):
     debug_log_in = False
     debug_errors = False
     is_login_widget = True
+    
+    recovery_email_template = """
+From: %(from_email)s
+To: %(to_email)s
+Subject: Recovered account information
+
+You have requested to recover your account information. Your password
+has been reset.
+
+Your username: %(username)s
+Your new password: %(new_password)s
+
+Please log in and change your password as soon as possible.
+"""
+    from_email = 'no-reply@localhost'
+
+    mail_host = 'localhost'
+    mail_username = None
+    mail_password = None
 
     def __init__(self, session, win_id, **attrs):
         Webwidgets.Html.__init__(self, session, win_id, **attrs)
@@ -34,23 +53,86 @@ class LogIn(object):
   
     class LogIn(object):
         def selected(self, path, value):
-            fields = self.get_widgets_by_attribute('field_name')
+            if path != self.path: return
             login_widget = self.get_ansestor_by_attribute("is_login_widget", True)
+            if value == 'login':            
+                fields = self.get_widgets_by_attribute('field_name')
 
-            if login_widget.debug_log_in: print "Log in attempt:", fields['username'].value, fields['password'].value
+                if login_widget.debug_log_in: print "Log in attempt:", fields['username'].value, fields['password'].value
 
-            try:
-                login_widget.user_info = login_widget.authenticate(
-                    fields['username'].value,
-                    fields['password'].value)
-            except Exception, e:
-                if login_widget.debug_errors: traceback.print_exc()
-                fields['username'].error = unicode(e)
-            else:
-                if login_widget.debug_log_in: print "User logged in:", login_widget.user_info
-                
+                try:
+                    login_widget.user_info = login_widget.authenticate(
+                        fields['username'].value,
+                        fields['password'].value)
+                except Exception, e:
+                    if login_widget.debug_errors: traceback.print_exc()
+                    fields['username'].error = unicode(e)
+                else:
+                    if login_widget.debug_log_in: print "User logged in:", login_widget.user_info
+            if value == 'forgotten':
+                Webwidgets.DialogContainer.add_dialog_to_nearest(
+                    self,
+                    self.Forgotten(self.session, self.win_id, login_widget = login_widget))
+
+        class Forgotten(object):
+             class Body(object):
+                 class IdentitiesGroup(object):
+                     class Username(object):
+                         class Field(object):
+                             invalid_username_or_email = "You must provide either a username or an email address."
+                             def validate_username_or_email(self):
+                                 return self.value or self.parent.parent['EMail']['Field'].value
+             
+             def selected(self, path, value):
+                 if path != self.path: return
+                 if not self.validate(): return
+                 if value == "recover":
+                     fields = self.get_widgets_by_attribute('field_name')
+                     username = fields['recover_username'].value
+                     email = fields['recover_email'].value
+                     
+                     if not username:
+                         username = self.login_widget.get_username_from_email(email)
+                     elif not email:
+                         email = self.login_widget.get_email_from_username(username)
+
+                     if not (username and email):
+                         result_dialog = self.RecoveryFailed
+                     else:
+                         result_dialog = self.RecoveryComplete
+                         new_password = self.login_widget.reset_password_for_user(username)
+
+                         smtp = smtplib.SMTP()
+                         smtp.connect(self.login_widget.mail_host)
+                         if self.login_widget.mail_username and self.login_widget.mail_password:
+                             smtp.login(self.login_widget.mail_username, self.login_widget.mail_password)
+
+                         email_content = self.login_widget.recovery_email_template % {
+                             'from_email': self.login_widget.from_email,
+                             'to_email': email,
+                             'username': username,
+                             'new_password': new_password}
+                         smtp.sendmail(
+                             self.login_widget.from_email, email,
+                             email_content)
+                         smtp.quit()
+
+                     Webwidgets.DialogContainer.add_dialog_to_nearest(
+                         self,
+                         result_dialog(self.session, self.win_id))
+                 Webwidgets.Dialog.selected(self, path, value)
+
     def authenticate(self, username, password):
-        raise Exception("You must override the authenticate() method of this widget!")
+        raise NotImplementedError("You must override the authenticate() method of this widget!")
+
+    def get_username_from_email(self, email):
+        raise NotImplementedError("You must override the get_username_from_email() method of this widget!")
+    
+    def get_email_from_username(self, username):
+        raise NotImplementedError("You must override the get_email_from_username() method of this widget!")
+    
+    def reset_password_for_user(self, username):
+        raise NotImplementedError("You must override the reset_password_for_user() method of this widget!")
 
     def user_info_changed(self, path, value):
         if self.global_session:
