4eea0b26d57c3c0692d639216415c3a5415891ca
nooslilaxe <nooslilaxe>
2008-11-26 14:54:26 +0000

    Generalize performance report to accept arbitrary measurement types, not only sql

--- Utils/Performance.py
+++ Utils/Performance.py
@@ -8,15 +8,22 @@ def start_report():
     perf_data.start = True
     perf_data.miss = 0
     perf_data.hit  = 0
-    perf_data.sql  = [];
     perf_data.start_time = datetime.datetime.now()
     perf_data.attrs = {}
+    perf_data.added = {}
 
-def add_sql(time, sql):
+def add_sql(time, text):
+    add('sql', text, time)
+
+def add(perf_type, text, time):
     if 'start' not in dir(perf_data) or not perf_data.start :
         return
-    perf_data.sql.append( {'id':len(perf_data.sql), 'time': time, 'sql': cgi.escape(sql), 'traceback': cgi.escape("".join(traceback.format_stack())) })
-#    perf_data.sql.append( (time, sql + "\n\n\n" + "\n".join(traceback.format_stack()) ))
+    if perf_type not in dir(perf_data):
+        setattr(perf_data,perf_type,[])
+
+    perf_data.added[perf_type] = perf_type
+    
+    getattr(perf_data,perf_type).append( {'id':len(getattr(perf_data,perf_type)), 'time': time, 'text': cgi.escape(text), 'traceback': cgi.escape("".join(traceback.format_stack())) })
 
 def add_miss():
     if 'start' not in dir(perf_data) or not perf_data.start :
@@ -44,13 +51,13 @@ class NoReportAvailable(Exception):
 def get_report():
     if 'start' not in dir(perf_data) or not perf_data.start :
         raise NoReportAvailableError
-    if not len(perf_data.sql) and perf_data.miss==0 and perf_data.hit==0:
+    if not len(perf_data.added) and perf_data.miss==0 and perf_data.hit==0:
         return None
 
     res = """
 <html>
         <head>
-<script>
+		<script>
 
 function toggleVisible(el)
 {
@@ -63,32 +70,51 @@ function toggleVisible(el)
     }
 }
 
-</script>
+		</script>
         </head>
         <body>
-                <h1>Performance report</h1>"""
+                <h1>Performance report</h1>
+"""
 
     t2 = datetime.datetime.now()
     dt = t2-perf_data.start_time
     woot = dt.seconds + 0.000001* dt.microseconds
 
     res += "Total rendering time: %.1f s<br/>" % woot
-    res += "Total query time: %.1f s<br/>" % reduce(lambda x, y: x+y['time'], perf_data.sql, 0.0)
-    res += "Total number of queries: %d<br/>" % len(perf_data.sql)
+    for perf_type in perf_data.added:
+        res += "Total %s time: %.1f s<br/>" % (perf_type, reduce(lambda x, y: x+y['time'], getattr(perf_data,perf_type), 0.0))
+        res += "Total number of %s items: %d<br/>" % (perf_type, len(getattr(perf_data,perf_type)))
 
-    res += "<hr>Cache hits: %s <br/>Cache misses: %s<br/>" % (perf_data.hit, perf_data.miss)
+    res += """
+		<hr/>
+                Cache hits: %s <br/>
+                Cache misses: %s<br/>
+""" % (perf_data.hit, perf_data.miss)
 
-    for i in sorted(perf_data.sql,lambda x, y: cmp(x['time'],y['time']),reverse=True):
-        res += """<hr/>
-        Query time: %(time).4f s<br/>
-        Query:<pre>%(sql)s</pre>
-        Traceback:<button onclick="toggleVisible(getElementById('traceback_%(id)d'));">+</button><pre id='traceback_%(id)d' style='display: none;'>%(traceback)s</pre>""" % i
-
-#    res += "<hr/>\nAttribute access statistics:\n"
-#    for i in sorted(map(lambda x: (x[1],x[0]), perf_data.attrs.iteritems()), reverse=True):
-#        res += "Count: %d name:%s<br/>\n" % i
+    for perf_type in perf_data.added:
+        res +=  """
+                <h2>Details for %s</h2>
+                """ % perf_type
+        for i in sorted(getattr(perf_data,perf_type),lambda x, y: cmp(x['time'],y['time']),reverse=True):
+            res += """
+		<hr/>
+		Query time: %(time).4f s<br/>
+		Query:<pre>%(text)s</pre>
+		Traceback:
+		<button onclick="toggleVisible(getElementById('traceback_%(id)d'));">+</button>
+		<pre id='traceback_%(id)d' style='display: none;'>%(traceback)s</pre>""" % i
 
     res += "</body></html>"
     perf_data.start=False
     return res
 
+
+
+if __name__ == '__main__':
+    print "Testing performance library"
+    start_report()
+    add('sql', 'select hej', 0.1)
+    add('sql', 'select hopp', 0.3)
+    add('ldap', 'axel', 0.1)
+    add('ldap', 'claes', 0.001)
+    print get_report()
